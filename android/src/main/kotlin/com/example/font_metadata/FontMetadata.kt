188319019db15c9338cdc0986f1822913d911f7f
Mountains <Mountains97@foxmail.com>
2024-12-25 12:00:18 +0800

    fix: 中文fontFamily支持

--- android/src/main/kotlin/com/example/font_metadata/FontMetadata.kt
+++ android/src/main/kotlin/com/example/font_metadata/FontMetadata.kt
@@ -1,3 +1,5 @@
+import org.apache.fontbox.ttf.NameRecord
+import org.apache.fontbox.ttf.NamingTable
 import org.apache.fontbox.ttf.OTFParser
 import org.apache.fontbox.ttf.TTFParser
 import java.io.File
@@ -9,7 +11,10 @@ class FontMetadata {
             val ttfParser = TTFParser()
             val font = ttfParser.parse(file)
             val naming = font.naming
-            val fontFamily = naming.fontFamily
+            var fontFamily = naming.fontFamily
+            if (fontFamily.isNullOrEmpty()) {
+                fontFamily = getFontFamily(naming)
+            }
             font.close()
             return fontFamily
         } catch (e: Exception) {
@@ -20,7 +25,10 @@ class FontMetadata {
             val otfParser = OTFParser()
             val font = otfParser.parse(filePath)
             val naming = font.naming
-            val fontFamily = naming.fontFamily
+            var fontFamily = naming.fontFamily
+            if (fontFamily.isNullOrEmpty()) {
+                fontFamily = getFontFamily(naming)
+            }
             font.close()
             return fontFamily
         } catch (e: Exception) {
@@ -28,5 +36,16 @@ class FontMetadata {
         }
 
         return null
+    }
+
+    private fun getFontFamily(namingTable: NamingTable): String {
+        for (nameRecord in namingTable.nameRecords) {
+            if (nameRecord.nameId == NameRecord.NAME_FONT_FAMILY_NAME) {
+                if (!nameRecord.string.isNullOrEmpty()) {
+                    return nameRecord.string
+                }
+            }
+        }
+        return ""
     }
 }
